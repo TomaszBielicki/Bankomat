40765127959fbc7e746a180e991493291a4d8810
tomaszbielicki <tomek.bielicki@gmail.com>
2019-11-11 13:23:26 +0100

    Transfer method is done.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,8 +1,7 @@
 public class BankAccount {
 
-     String userName;
-     double accBalance;
-
+    String userName;
+    double accBalance;
 
 
     public BankAccount(String userName, double accBalance) {
@@ -12,20 +11,20 @@ public class BankAccount {
 
     }
 
-    public BankAccount (String userName){
+    public BankAccount(String userName) {
 
         this.userName = userName;
         this.accBalance = 0;
     }
 
-    public double deposit (double amounts){
+    public double deposit(double amounts) {
         if (0 < amounts) {
             this.accBalance += amounts;
         }
         return amounts;
     }
 
-    public void withdraw (double amounts) {
+    public void withdraw(double amounts) {
         if (!(accBalance < amounts))
             this.accBalance -= amounts;
         else
@@ -33,27 +32,30 @@ public class BankAccount {
     }
 
     // TODO Platnosc pomiedzy dwoma uzytkwnikami (kluczowy bedzie parametr Bankccount)
-    public void transfer (double amounts, BankAccount acc){
+    public void transfer(double amounts, BankAccount acc) {
 
-        if(!(accBalance > 0)) {
+        if (!(accBalance < amounts)) {
             accBalance -= amounts;
             acc.accBalance += amounts;
-        }
-        else {
+            System.out.println("Pieniadze w ilosci " + amounts + " zostaly przelane na konto uzytkownika " + acc);
+        } else {
             System.out.println("Za malo srodkow na przelew");
         }
-
-
-
     }
 
-   public String getUserName (){return userName;}
-   public void setUserName(String userName) {
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
         this.userName = userName;
     }
 
 
-    public double getAccBalance(){return accBalance;}
+    public double getAccBalance() {
+        return accBalance;
+    }
+
     public void setAccBalance(double accBalance) {
         this.accBalance = accBalance;
     }
--- src/Test.java
+++ src/Test.java
@@ -2,26 +2,26 @@ public class Test {
 
     public static void main(String[] args) {
 
-       // User user = new User("Tomek", 100);
-       // User user1 = new User ("Aga", 200);
+        // User user = new User("Tomek", 100);
+        // User user1 = new User ("Aga", 200);
 
-       // System.out.println(user.getName());
-       // System.out.println(user.getAccBalance());
+        // System.out.println(user.getName());
+        // System.out.println(user.getAccBalance());
 
         BankAccount tomek = new BankAccount("Tomek", 4000);
         BankAccount aga = new BankAccount("aga");
 
-        tomek.transfer(5000,aga);
+        tomek.transfer(400, aga);
         //aga.deposit(200);
         //tomek.withdraw(400);
         //aga.deposit(1000);
         System.out.println(tomek.toString());
         System.out.println(aga.toString());
 
-        }
+    }
 
 
-        }
+}
 
 
 
